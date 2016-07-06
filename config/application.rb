1f2b76288e034a120ecc021c5cfd2dbd8ef3b43c
Shinji KOBAYASHI <skoba@moss.gr.jp>
2016-07-06 19:37:22 +0900

    added default rspec configuration for rails

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -18,8 +17,13 @@ Bundler.require(*Rails.groups)
 
 module OpenehrTermservRails
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    g.test_framework :rspec,
+                     fixtures: true,
+                     view_specs: false,
+                     helper_specs: false,
+                     routing_specs: false,
+                     controller_specs: true,
+                     request_specs: false
+    g.fixture_replacement :factory_girl, dir: "spec/factories"   
   end
 end
