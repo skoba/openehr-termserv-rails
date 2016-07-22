9068d019c0ec45d633494d17a3c1756e418baf52
Shinji KOBAYASHI <skoba@moss.gr.jp>
2016-07-22 12:11:04 +0900

    made skelton

---
+++ app/controllers/concept_controller.rb
@@ -0,0 +1,7 @@
+class ConceptController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'concept/show'
+
+  get 'concept/index'
+
   resources :codes, only: [:index, :show]
 end
