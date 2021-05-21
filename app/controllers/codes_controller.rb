a926dd1dd2ca947061bc787e9a90aeb9815a06b6
Kobayashi Shinji <skoba@moss.gr.jp>
2021-05-21 11:21:39 +0900

    changed port

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -2,10 +2,11 @@ class CodesController < ApplicationController
   def index
     if code_params.empty?
       @codes = Code.all
-    else
-      @codes = Code.where(value: code_params[:code]).or(Code.where(description: code_params[:description]))
+    elsif code_params[:code]
+      @codes = Code.where(value: code_params[:code])
+    elsif code_params[:description]
+      Code.where(description: code_params[:description])
     end
-
     render 'index', formats: :json, handlers: 'jbuilder'
   end
 
